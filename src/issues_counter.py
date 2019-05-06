a24dc16c8303697fc93904bd39e0ca50f2a63b87
annaVR <anna@kirsanova.name>
2019-05-05 22:42:42 -0700

    update

--- src/issues_counter.py
+++ src/issues_counter.py
@@ -8,6 +8,7 @@ def main():
     :return: timestamp and issue count per day.
     """
     json_list = get_json_list()
+    if setup
     if json_list:
         for json_day in json_list:
             issues_counter = 0
